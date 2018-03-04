94fd9bc43b98bb06feab6743d370a1c04f74faa6
haishenghui <haishenghui@163.com>
2018-03-04 13:33:56 +0800

    ad

--- h5/js/common.js
+++ h5/js/common.js
@@ -357,7 +357,7 @@ $(function(){
 	if(!isAuth){
 		if(!GetQueryString('isauth')){
 			var href = window.location.href
-			//location.href="http://yichuanglian.huimor.com/index?fromurl="+href;
+			location.href="http://yichuanglian.huimor.com/index?fromurl="+href;
 		}else{
 			localStorage.setItem('isauth',1);
 		}
